e4d5dac60b172610468b0b517b33313c030c34af
DingjieYang <dingjiedanielyang@gmail.com>
2022-05-26 13:02:53 -0700

    Remove CSRF protection for Login Page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 #include ArcticleProssess
 
-  skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token
 
   def index
     @articles = Article.all
